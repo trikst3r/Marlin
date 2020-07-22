c5443b84d7b15c58ed04e3e58afab62f7c26d6c2
trikst3r <mail4thespam@gmail.com>
2020-07-22 18:23:34 +0300

    Calibrated PID values OEM hotend

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -491,10 +491,10 @@
 
   // If you are using a pre-configured hotend then you can use one of the value sets by uncommenting it
 
-  // Ultimaker
-  #define DEFAULT_Kp 22.2
-  #define DEFAULT_Ki 1.08
-  #define DEFAULT_Kd 114
+  // CR-10S OEM hotend
+  #define DEFAULT_Kp 45.03
+  #define DEFAULT_Ki 5.83
+  #define DEFAULT_Kd 86.93
 
   // MakerGear
   //#define DEFAULT_Kp 7.0
